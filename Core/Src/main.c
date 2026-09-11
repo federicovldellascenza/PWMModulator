5e2f2d6e0ce3e8c8dd4dd470c95518f0a5301c06
Federico Vittorio Lucio Dell'Ascenza <federico.dellascenza@unicas.it>
2026-09-11 15:42:26 +0200

    commenti aggiornati

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -121,7 +121,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    // L'intero loop è delegato alla FSM
+    // L'intero loop è delegato alla FSM, check fsm.c
     FSM_Process();
       
   }
